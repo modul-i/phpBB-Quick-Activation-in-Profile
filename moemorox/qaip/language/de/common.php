b95465a3e1c7d47145396a4ab263fb4990e35a99
Joe Kolade <152904953+modul-i@users.noreply.github.com>
2025-04-15 21:29:17 +0200

    Code formatting

--- moemorox/qaip/language/de/common.php
+++ moemorox/qaip/language/de/common.php
@@ -1,12 +1,12 @@
 <?php
 
 if (!defined('IN_PHPBB')) {
-    exit;
+	exit;
 }
 
 $lang = array_merge($lang, array(
-    'USER_ACTIVATE' => 'Benutzer aktivieren',
+	'USER_ACTIVATE' => 'Benutzer aktivieren',
 	'ADMINS_AREA' => 'Infobereich für Administratoren',
-    'ONLY_VISIBLE_FOR_ADMINS' => 'nur für Administrator(en) sichtbar',
-    'LAST_IP' => 'Letzte IP',
+	'ONLY_VISIBLE_FOR_ADMINS' => 'nur für Administrator(en) sichtbar',
+	'LAST_IP' => 'Letzte IP',
 ));
--- moemorox/qaip/language/en/common.php
+++ moemorox/qaip/language/en/common.php
@@ -1,12 +1,12 @@
 <?php
 
 if (!defined('IN_PHPBB')) {
-    exit;
+	exit;
 }
 
 $lang = array_merge($lang, array(
-    'USER_ACTIVATE' => 'Activate user',
+	'USER_ACTIVATE' => 'Activate user',
 	'ADMINS_AREA' => 'Info area for administrators',
-    'ONLY_VISIBLE_FOR_ADMINS' => 'Only visible for admin(s)',
-    'LAST_IP' => 'Last IP',
+	'ONLY_VISIBLE_FOR_ADMINS' => 'Only visible for admin(s)',
+	'LAST_IP' => 'Last IP',
 ));
