e6e2827039f2ccdcc3febfeaad9722c518c6432f
Joe Kolade <152904953+modul-i@users.noreply.github.com>
2025-04-19 10:20:58 +0200

    added language files

---
+++ moemorox/qaip/language/nl/common.php
@@ -0,0 +1,12 @@
+<?php
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+$lang = array_merge($lang, array(
+	'USER_ACTIVATE' => 'Benutzer aktivieren',
+	'ADMINS_AREA' => 'Infobereich für Administratoren',
+	'LAST_IP' => 'IP-Adresse bei Registrierung',
+));
