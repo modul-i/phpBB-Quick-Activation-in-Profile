877d7d0443d7a9e6fe83512f2438997cef6be967
Joe Kolade <152904953+modul-i@users.noreply.github.com>
2025-04-20 10:14:47 +0200

    Update ext.php

--- moemorox/qaip/ext.php
+++ moemorox/qaip/ext.php
@@ -2,4 +2,13 @@
 
 namespace moemorox\qaip;
 
-class ext extends \phpbb\extension\base {}
+class ext extends \phpbb\extension\base
+{
+	public function is_enableable()
+	{
+		$valid_phpbb = phpbb_version_compare(PHPBB_VERSION, '3.3.0', '>=') && phpbb_version_compare(PHPBB_VERSION, '4.0-dev', '<');
+		$valid_php = phpbb_version_compare(PHP_VERSION, '7.2.0', '>=') && phpbb_version_compare(PHP_VERSION, '8.5.0-dev', '<');
+
+		return $valid_phpbb && $valid_php;
+	}
+}
