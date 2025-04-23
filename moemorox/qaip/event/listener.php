a09c18670ffc4de3fbf42a5e0ee0642ec158254b
Joe Kolade <152904953+modul-i@users.noreply.github.com>
2025-04-23 20:30:52 +0200

    Optimizations and bugfix

--- moemorox/qaip/event/listener.php
+++ moemorox/qaip/event/listener.php
@@ -42,96 +42,95 @@ class listener implements EventSubscriberInterface
 		];
 	}
 
-	public function memberlist_modify_view_profile_template_vars()
-	{
-		$this->add_activate_url();
-		$this->inject_email();
-		$this->inject_website();
-		$this->inject_ip();
-	}
-
-	public function add_activate_url()
+	public function memberlist_modify_view_profile_template_vars($event)
 	{
 		// Admin check
-		if (!$this->user->data['is_registered'] || !($this->auth->acl_get('a_') || $this->user->data['user_type'] == USER_FOUNDER))
+		if (!$this->admin_check())
 		{
 			return;
 		}
 
-		$user_id = (int) $this->request->variable('u', 0);
+		$user_id = $event['user_id'] ?? $this->request->variable('u', 0);
 		if (empty($user_id))
 		{
 			return;
 		}
 
-		// Get user_actkey from DB
-		$sql = 'SELECT user_actkey FROM ' . USERS_TABLE . ' WHERE user_id = ' . $this->db->sql_escape($user_id) . ' AND user_type = 1;';
+		$user_data = $this->get_user_data($user_id);
+
+		$this->add_activate_url($user_id, $user_data);
+		$this->inject_email($user_data);
+		$this->inject_ip($user_data);
+		$this->inject_website($user_id);
+	}
+
+	private function admin_check()
+	{
+		if (!$this->user->data['is_registered'] || !($this->auth->acl_get('a_') || $this->user->data['user_type'] == USER_FOUNDER))
+		{
+			return false;
+		}
+
+		return true;
+	}
+
+	private function get_user_data($user_id)
+	{
+		$sql = 'SELECT user_actkey, user_type, user_email, user_ip FROM ' . USERS_TABLE . ' WHERE user_id = ' . (int) $user_id;
 		$result = $this->db->sql_query($sql);
 		$row = $this->db->sql_fetchrow($result);
 		$this->db->sql_freeresult($result);
 
-		if (!empty($row['user_actkey']))
+		return $row;
+	}
+
+	private function add_activate_url($user_id, $user_data)
+	{
+		// Check if user is already activated
+		if (empty($user_data['user_type']) || (int) $user_data['user_type'] !== 1)
 		{
-			$activate_link = generate_board_url() . "/ucp.php?mode=activate&u={$user_id}&k=" . $row['user_actkey'];
+			return;
+		}
+
+		if (!empty($user_data['user_actkey']))
+		{
+			$activate_link = generate_board_url() . "/ucp.php?mode=activate&u={$user_id}&k=" . $user_data['user_actkey'];
 
 			$this->template->assign_vars([
 				'U_ACTIVATE' => $activate_link,
-				'L_USER_ACTIVATE' => $this->language->lang('USER_ACTIVATE'),
 			]);
 		}
 	}
 
-	public function inject_email()
+	private function inject_email($user_data)
 	{
-		if (
-			!$this->user->data['is_registered']
-			|| !($this->auth->acl_get('a_') || $this->user->data['user_type'] == USER_FOUNDER)
-		)
-		{
-			return;
-		}
-
-		$user_id = (int) $this->request->variable('u', 0);
-		if (empty($user_id))
-		{
-			return;
-		}
-
-		$sql = 'SELECT user_email FROM ' . USERS_TABLE . ' WHERE user_id = ' . $this->db->sql_escape($user_id);
-		$result = $this->db->sql_query($sql);
-		$row = $this->db->sql_fetchrow($result);
-		$this->db->sql_freeresult($result);
-
-		if (!empty($row['user_email']))
+		if (!empty($user_data['user_email']))
 		{
 			$this->template->assign_vars([
-				'USER_REAL_EMAIL' => $row['user_email'],
+				'USER_REAL_EMAIL' => $user_data['user_email'],
 			]);
 		}
 	}
 
-	public function inject_website()
+	private function inject_ip($user_data)
 	{
-		if (
-			!$this->user->data['is_registered']
-			|| !($this->auth->acl_get('a_') || $this->user->data['user_type'] == USER_FOUNDER)
-		)
+		if (!empty($user_data['user_ip']))
 		{
-			return;
+			$this->template->assign_vars([
+				'USER_IP' => $user_data['user_ip'],
+				'USER_IP_URL' => 'https://ipinfo.io/' . $user_data['user_ip'],
+			]);
 		}
+	}
 
+	private function inject_website($user_id)
+	{
 		if (!$this->db_tools->sql_column_exists(PROFILE_FIELDS_DATA_TABLE, 'pf_phpbb_website'))
 		{
 			return;
 		}
 
-		$user_id = (int) $this->request->variable('u', 0);
-		if (empty($user_id))
-		{
-			return;
-		}
-
-		$sql = 'SELECT pf_phpbb_website FROM ' . PROFILE_FIELDS_DATA_TABLE . ' WHERE user_id = ' . $this->db->sql_escape($user_id);
+		$sql = 'SELECT pf_phpbb_website FROM ' . PROFILE_FIELDS_DATA_TABLE . ' WHERE user_id = ' . (int) $user_id;
 		$result = $this->db->sql_query($sql);
 		$row = $this->db->sql_fetchrow($result);
 		$this->db->sql_freeresult($result);
@@ -140,36 +139,6 @@ class listener implements EventSubscriberInterface
 		{
 			$this->template->assign_vars([
 				'USER_WEBSITE_RAW' => $row['pf_phpbb_website'],
-			]);
-		}
-	}
-
-	public function inject_ip()
-	{
-		if (
-			!$this->user->data['is_registered']
-			|| !($this->auth->acl_get('a_') || $this->user->data['user_type'] == USER_FOUNDER)
-		)
-		{
-			return;
-		}
-
-		$user_id = (int) $this->request->variable('u', 0);
-		if (empty($user_id))
-		{
-			return;
-		}
-
-		$sql = 'SELECT user_ip FROM ' . USERS_TABLE . ' WHERE user_id = ' . $this->db->sql_escape($user_id);
-		$result = $this->db->sql_query($sql);
-		$row = $this->db->sql_fetchrow($result);
-		$this->db->sql_freeresult($result);
-
-		if (!empty($row['user_ip']))
-		{
-			$this->template->assign_vars([
-				'USER_IP' => $row['user_ip'],
-				'USER_IP_URL' => 'https://ipinfo.io/' . $row['user_ip'],
 			]);
 		}
 	}
